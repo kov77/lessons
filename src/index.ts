ce8fb6d7b28a663df16369407ddf88f5fed68d34
Maksym  Koval <maksym6167@gmail.com>
2025-04-19 11:38:39 -0400

    refactor: using router for tests endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import { Response } from "express";
 import { setupApp } from "./setup-app";
-import { db } from "./db/db";
 import { videosRouter } from "./routes/videos.router";
+import { testsRouter } from "src/routes/tests.router";
 
 export const app = express();
 setupApp(app);
@@ -14,8 +13,4 @@ app.listen(PORT, () => {
 });
 
 app.use("/hometask_01/api/videos", videosRouter);
-
-app.delete("/hometask_01/api/testing/all-data", (_, res: Response) => {
-  db.videos = [];
-  res.sendStatus(204).send("All data is deleted");
-});
+app.use("/hometask_01/api/testing", testsRouter);
