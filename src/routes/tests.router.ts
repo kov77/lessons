eec03291a266d3d1f712f6422d5005bbf06dac91
kov77 <maksim6167@gmail.com>
2025-04-21 11:10:18 -0400

    fix: add types and migrate to express 4 version

--- src/routes/tests.router.ts
+++ src/routes/tests.router.ts
@@ -1,9 +1,9 @@
-import { Response, Router } from "express";
+import { Response, Request, Router } from "express";
 import { db } from "../db/db";
 
 export const testsRouter = Router();
 
-testsRouter.delete("/all-data", (_, res: Response) => {
+testsRouter.delete("/all-data", (req: Request, res: Response) => {
   db.videos = [];
   res.sendStatus(204);
 });
