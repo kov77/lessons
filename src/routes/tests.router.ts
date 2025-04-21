f22affcabf81e4be396e22bb6e0f74de4ec39e21
kov77 <maksim6167@gmail.com>
2025-04-20 22:17:15 -0400

    fix: add missing types to express handlers for Vercel compatibility

--- src/routes/tests.router.ts
+++ src/routes/tests.router.ts
@@ -5,5 +5,5 @@ export const testsRouter = Router();
 
 testsRouter.delete("/all-data", (_, res: Response) => {
   db.videos = [];
-  res.sendStatus(204).send("All data is deleted");
+  res.sendStatus(204);
 });
