2b7b2700dcacfd6ff8ea3011b6c6e2793298319d
kov77 <maksim6167@gmail.com>
2025-04-21 13:48:49 -0400

    fix: fix app export

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { testsRouter } from "./routes/tests.router";
 import { videosRouter } from "./routes/videos.router";
 import { setupApp } from "./setup-app";
 
-export const app = express();
+const app = express();
 setupApp(app);
 
 app.use("/hometask_01/api/videos", videosRouter);
 app.use("/hometask_01/api/testing", testsRouter);
+
+export default app;
