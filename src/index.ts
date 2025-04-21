883d29a1d7469faf6a8df7a0202702e4f85a546d
kov77 <maksim6167@gmail.com>
2025-04-20 22:06:18 -0400

    fix: update imports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
 import express from "express";
-import { setupApp } from "./setup-app";
+import { testsRouter } from "./routes/tests.router";
 import { videosRouter } from "./routes/videos.router";
-import { testsRouter } from "src/routes/tests.router";
+import { setupApp } from "./setup-app";
 
 export const app = express();
 setupApp(app);
-
-const PORT = process.env.PORT || 5001;
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
-});
 
 app.use("/hometask_01/api/videos", videosRouter);
 app.use("/hometask_01/api/testing", testsRouter);
